da562b9e0ebfb67270b237daeff4bb36859ff407
ImSkoot <jamesscottbaronia@gmail.com>
2024-09-19 21:46:52 +0800

    update hashing.py
    made it to hash specific user input.

--- hashing.py
+++ hashing.py
@@ -13,23 +13,10 @@ user_input = input("Input your password: ")
 h = hashlib.new("SHA256")
 h.update(user_input.encode())
 
-hashed_password = h.hexdigest()
+hashed = h.hexdigest()
 
-print(f"Your password {user_input} is hashed to {hashed_password}.")
+print(f"Your password {user_input} is hashed to {hashed}.")
 
-
-checked_login = input("Input your password again: ")
-
-h2 = hashlib.new("SHA256")
-
-h2.update(checked_login.encode())
-
-login_hash = h2.hexdigest()
-
-if login_hash == hashed_password:
-    print("You've been signed in.")
-else: 
-    print("Incorrect password.")
 
 
 
