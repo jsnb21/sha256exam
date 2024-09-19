942794e1db32059613b86940dc16e2f504c9db17
ImSkoot <jamesscottbaronia@gmail.com>
2024-09-19 21:34:50 +0800

    updated hashing.py

--- hashing.py
+++ hashing.py
@@ -8,8 +8,29 @@ Binalla, Carmela
 
 '''
 
-h = hashlib.new("SHA256")
-h.update(b"Hello World")
+user_input = input("Input your password: ")
 
-print(h.hexdigest())
+h = hashlib.new("SHA256")
+h.update(user_input.encode())
+
+hashed_password = h.hexdigest()
+
+print(f"Your password {user_input} is hashed to {hashed_password}.")
+
+
+checked_login = input("Input your password again: ")
+
+h2 = hashlib.new("SHA256")
+
+h2.update(checked_login.encode())
+
+login_hash = h2.hexdigest()
+
+if login_hash == hashed_password:
+    print("You've been signed in.")
+else: 
+    print("Incorrect password.")
+
+
+
 
