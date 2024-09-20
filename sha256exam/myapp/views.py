c01e99f7b50793c2b1ae4271f2f9ed3d68cbca03
ImSkoot <jamesscottbaronia@gmail.com>
2024-09-20 22:19:23 +0800

    Implemented SHA-256 input and output

--- sha256exam/myapp/views.py
+++ sha256exam/myapp/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render, HttpResponse
+import hashlib
 
 # Create your views here.
 def home(request):
-    return render(request, "home.html")
+    input_data = None
+    hashed_value = None
+    
+    if request.method == 'POST':
+        input_data = request.POST.get('inputData')
+        
+        if input_data:
+            hash_object = hashlib.sha256(input_data.encode())
+            hashed_value = hash_object.hexdigest()
+    
+    return render(request, 'home.html', {'input_data': input_data, 'hashed_value': hashed_value})
